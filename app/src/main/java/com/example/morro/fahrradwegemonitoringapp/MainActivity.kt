0d4a2fbd6a880a5a50d374beeeb54c5ef445e9f6
Julian Magierski <julian.magierski@haw-hamburg.de>
2018-01-23 22:52:58 +0100

    add toggle button

--- app/src/main/java/com/example/morro/fahrradwegemonitoringapp/MainActivity.kt
+++ app/src/main/java/com/example/morro/fahrradwegemonitoringapp/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.morro.fahrradwegemonitoringapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toggle = findViewById(R.id.toggleButton) as ToggleButton
+        toggle.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener {
+            override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
+                if (isChecked) {
+                    // The toggle is enabled
+                } else {
+                    // The toggle is disabled
+                }
+            }
+        })
     }
 }
