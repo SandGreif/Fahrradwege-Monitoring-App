e74fd0653befcf8721c8a204077e407ee3ba51f3
Julian Magierski <julian.magierski@haw-hamburg.de>
2018-01-24 13:57:53 +0100

    add basic camera2 api

--- app/src/main/java/com/example/morro/fahrradwegemonitoringapp/MainActivity.kt
+++ app/src/main/java/com/example/morro/fahrradwegemonitoringapp/MainActivity.kt
@@ -1,24 +1,24 @@
-package com.example.morro.fahrradwegemonitoringapp
 
-import android.support.v7.app.AppCompatActivity
+package com.example.morro.fahrradwegemonitoringapp
 import android.os.Bundle
-import android.widget.CompoundButton
-import android.widget.ToggleButton
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
 
+    var savedInstanceStateV: Bundle? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val toggle = findViewById(R.id.toggleButton) as ToggleButton
-        toggle.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener {
-            override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
-                if (isChecked) {
-                    // The toggle is enabled
-                } else {
-                    // The toggle is disabled
-                }
-            }
-        })
+        setContentView(R.layout.activity_camera)
+        savedInstanceStateV = savedInstanceState;
     }
+
+    fun startBildaufnahme(view: View){
+        savedInstanceStateV ?: supportFragmentManager.beginTransaction()
+                 .replace(R.id.container, Camera2BasicFragment.newInstance())
+                 .commit()
+    }
+
 }
